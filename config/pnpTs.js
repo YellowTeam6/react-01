4cdb487a19a4f1af97df5de2a46f1915ff6df14e
LiuBaoy <53083777+LiuBaoy@users.noreply.github.com>
2019-11-04 13:01:46 +0800

    Revert "Add files via upload"

--- config/pnpTs.js
+++ config/pnpTs.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 const { resolveModuleName } = require('ts-pnp');
@@ -34,40 +33,3 @@ exports.resolveTypeReferenceDirective = (
     typescript.resolveTypeReferenceDirective
   );
 };
-=======
-'use strict';
-
-const { resolveModuleName } = require('ts-pnp');
-
-exports.resolveModuleName = (
-  typescript,
-  moduleName,
-  containingFile,
-  compilerOptions,
-  resolutionHost
-) => {
-  return resolveModuleName(
-    moduleName,
-    containingFile,
-    compilerOptions,
-    resolutionHost,
-    typescript.resolveModuleName
-  );
-};
-
-exports.resolveTypeReferenceDirective = (
-  typescript,
-  moduleName,
-  containingFile,
-  compilerOptions,
-  resolutionHost
-) => {
-  return resolveModuleName(
-    moduleName,
-    containingFile,
-    compilerOptions,
-    resolutionHost,
-    typescript.resolveTypeReferenceDirective
-  );
-};
->>>>>>> 2cf4215ce8cb6e61bee1b0250ded050a6ca784de
